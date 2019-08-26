3745dcafe479d2a801a7c1a0b07be0f0d81ac3c2
zbshadow <zbshadowx@gmail.com>
2019-08-26 06:44:36 -0400

    On branch master
    Changes to be committed:
    	modified:   app.js
    	modified:   index.html
    
    Fixed: Clock function moved to app.js and is no longer called in seperate script from HTML.
    Fixed: Clock now shows default location(Currently Los Angeles) time and date instead of local computers.

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ window.addEventListener('load', ()=>{
     const temperatureSpan = document.querySelector('.degree-section span');
 
     //New York, New York
-    long = -74.0060;
-    lat = 40.7128;
+    long = -118.2437;
+    lat = 34.0522;
     getWeather(long, lat);
     setLocation(long, lat);
 
@@ -47,6 +47,9 @@ window.addEventListener('load', ()=>{
                         temperatureDegree.textContent = Math.floor(temperature);
                     }
                 });
+                tz = data.timezone;
+                setClock(tz);
+                setInterval(setClock, 1000, tz);
             });
     }
 
@@ -76,7 +79,63 @@ window.addEventListener('load', ()=>{
                 })
 
         }
+
+        function setClock(tz){
+            console.log(tz);
+            const dateString = new Date().toLocaleString("en-US", {timeZone: `${tz}`});
+            const fullDate = new Date(dateString);
+            console.log(dateString);
+            console.log(fullDate);
+
+            var dow = new Array();
+            dow[0] = "Sunday";
+            dow[1] = "Monday";
+            dow[2] = "Tuesday";
+            dow[3] = "Wednesday";
+            dow[4] = "Thursday";
+            dow[5] = "Friday";
+            dow[6] = "Saturday";
+            var d = dow[fullDate.getDay()];
+            var month = new Array();
+            month[0] = "January";
+            month[1] = "February";
+            month[2] = "March";
+            month[3] = "April";
+            month[4] = "May";
+            month[5] = "June";
+            month[6] = "July";
+            month[7] = "August";
+            month[8] = "September";
+            month[9] = "October";
+            month[10] = "Novemeber";
+            month[11] = "December";
+            var m = month[fullDate.getMonth()];
+            var day = fullDate.getDate();
+            var hours = fullDate.getHours();
+            var mins = fullDate.getMinutes();
+
+
+
+            if (hours < 10){
+                hours = "0" + hours;
+            }
         
+            if (mins < 10) {
+                mins = "0" +mins;
+            }
+
+            document.getElementById('dow').innerHTML = d + ",";
+            document.getElementById('month').innerHTML = m;
+            document.getElementById('day').innerHTML = day;
+            document.getElementById('hour').innerHTML = hours + " :";
+            document.getElementById('minute').innerHTML = mins;
+
+            if (hours > 19 || hours < 7){
+                document.body.style.background ='linear-gradient(rgb(89,0,179), rgb(38,0,77))';
+            }
+
+        }
+
         function setIcons(icon, iconID) {
             const skycons = new Skycons({color: "white"});
             const currentIcon = icon.replace(/-/g, "_").toUpperCase();
