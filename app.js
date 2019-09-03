ec8f498f3d014b0a3180522c5f84a5851cba6764
zbshadow <zbshadowx@gmail.com>
2019-09-03 03:22:31 -0400

    On branch master
     Changes to be committed:
    	modified:   app.js
    	deleted:    appOld.js
    	deleted:    clock.js
    	modified:   index.html
    	modified:   style.css
    
    Functions moved to global scope of app.js rather than local to the on load event handler.
    This was done to allow for event handlers to use the functions with out each function being defined inside the event handler.
    Variables for functions have also been moved to the global scope of app.js so they are still accessible to the functions.
    
    Fixed:
    	- Default location comments in the old app.js file said 'New York, New York' the location was in fact Los Angeles, CA.
    	- localization of functions
    	- localization of variables
    
    Added:
    	- 'Use My Location' Button added to page to allow the user the option to click the button and allow location services.
    	- event handler that handles location button clicks
    	- CSS for location button
    	- function to stop clock updates(this allows location changes, in the past if location and time changes, the text would swich between values ~ every .5seconds.
    
    Removed:
    	- clock.js(unused after last update where the clock was moved ot a function in app.js
    	- appOld.js (left over from first rewrite of app.js)
    
    Comments:
    	Possible need to remove initial on load geolocation request, if the option to deny is selected then the button to 'Use My Location' will not do anything, this is a permissions issue with web browsers. Once denied you can not change the option.

--- app.js
+++ app.js
@@ -1,145 +1,191 @@
-window.addEventListener('load', ()=>{
-    let long;
-    let lat;
-    let temperatureDescription = document.querySelector('.temperature-description');
-    let temperatureDegree = document.querySelector('.temperature-degree');
-    let locationTimezone = document.querySelector('.location-timezone');
-    let temperatureSection = document.querySelector('.degree-section');
-    const temperatureSpan = document.querySelector('.degree-section span');
+let long;
+let lat;
+let temperatureDescription = document.querySelector('.temperature-description');
+let temperatureDegree = document.querySelector('.temperature-degree');
+let locationTimezone = document.querySelector('.location-timezone');
+let temperatureSection = document.querySelector('.degree-section');
+let locationButton = document.getElementById('locationButton');
+const temperatureSpan = document.querySelector('.degree-section span');
 
-    //New York, New York
-    long = -118.2437;
-    lat = 34.0522;
-    getWeather(long, lat);
-    setLocation(long, lat);
+function getLocation (){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            getWeather(long, lat);
+            setLocation(long, lat);
+        }, showDefault)
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+    function showDefault(){
+        long = -118.2437;
+        lat = 34.0522;
+        getWeather(long, lat);
+        setLocation(long, lat);
+    }
+}
 
-    function getWeather(long,lat){
-         //proxy needed for local host testing
-        const proxy = "https://cors-anywhere.herokuapp.com/";
-        const apiWeather = `${proxy}https://api.darksky.net/forecast/d6c54403d3a95091c4bafacaf12808cd/${lat},${long}`;
+function setLocation(long, lat) {
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    const apiLocation = `${proxy}https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=AIzaSyDgkz55qvQ6bG36xW7MUjY0ORVDYqCi-Mw`
+    fetch(apiLocation)
+        .then (fetched => {
+            return fetched.json();
+        })
+        .then(dataL => {
+            locationArray = dataL.results[0];
+            for (var i=0; i<locationArray.address_components.length; i++)
+            {
+                if (locationArray.address_components[i].types[0] == "locality"){
+                    city = locationArray.address_components[i];
+                }
+                if (locationArray.address_components[i].types[0] == "administrative_area_level_1"){
+                    region = locationArray.address_components[i];
+                }
+                if (locationArray.address_components[i].types[0] == "postal_code"){
+                    postal = locationArray.address_components[i];
+                }
+            }
+            locationTimezone.textContent = city.long_name + ", " + region.long_name;
+        })
 
-        fetch(apiWeather)
-            .then(fetched => {
-                return fetched.json();
-            })
-            .then(data => {
-                console.log(data);
-                const {temperature, summary, icon} = data.currently;
+}
 
-                //set DOM Elements from API
-                temperatureDegree.textContent = Math.floor(temperature);
-                temperatureDescription.textContent = summary;
-                //locationTimezone.textContent = data.timezone;
+function getWeather(long,lat){
+    //proxy needed for local host testing
+   const proxy = "https://cors-anywhere.herokuapp.com/";
+   const apiWeather = `${proxy}https://api.darksky.net/forecast/d6c54403d3a95091c4bafacaf12808cd/${lat},${long}`;
 
-                //formula for celsius
-                let celsius = (temperature - 32) * (5 / 9);
+   fetch(apiWeather)
+       .then(fetched => {
+           return fetched.json();
+       })
+       .then(data => {
+           console.log(data);
+           const {temperature, summary, icon} = data.currently;
 
-                //Set Icon
-                setIcons(icon, document.querySelector(".icon"));
+           //set DOM Elements from API
+           temperatureDegree.textContent = Math.floor(temperature);
+           temperatureDescription.textContent = summary;
+           //locationTimezone.textContent = data.timezone;
 
-                //Change temperature to C/F
-                temperatureSection.addEventListener('click', () =>{
-                    if(temperatureSpan.textContent === "F"){
-                        temperatureSpan.textContent = "C";
-                        temperatureDegree.textContent = Math.floor(celsius);
-                    }else{
-                        temperatureSpan.textContent = "F";
-                        temperatureDegree.textContent = Math.floor(temperature);
-                    }
-                });
-                tz = data.timezone;
-                setClock(tz);
-                setInterval(setClock, 1000, tz);
-            });
+           //formula for celsius
+           let celsius = (temperature - 32) * (5 / 9);
+
+           //Set Icon
+           setIcons(icon, document.querySelector(".icon"));
+
+           //Change temperature to C/F
+           temperatureSection.addEventListener('click', () =>{
+               if(temperatureSpan.textContent === "F"){
+                   temperatureSpan.textContent = "C";
+                   temperatureDegree.textContent = Math.floor(celsius);
+               }else{
+                   temperatureSpan.textContent = "F";
+                   temperatureDegree.textContent = Math.floor(temperature);
+               }
+           });
+           tz = data.timezone;
+           //setClock(tz);
+           testVar = setInterval(setClock, 1000, tz);
+           locationButton.addEventListener('click', ()=> {
+               clearInterval(testVar);
+           });
+       });
+}
+
+function setIcons(icon, iconID) {
+    const skycons = new Skycons({color: "white"});
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+    skycons.play();
+    return skycons.set(iconID, Skycons[currentIcon]);
+}
+
+function setClock(tz){
+    const dateString = new Date().toLocaleString("en-US", {timeZone: `${tz}`});
+    const fullDate = new Date(dateString);
+
+    var dow = new Array();
+    dow[0] = "Sunday";
+    dow[1] = "Monday";
+    dow[2] = "Tuesday";
+    dow[3] = "Wednesday";
+    dow[4] = "Thursday";
+    dow[5] = "Friday";
+    dow[6] = "Saturday";
+    var d = dow[fullDate.getDay()];
+    var month = new Array();
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "Novemeber";
+    month[11] = "December";
+    var m = month[fullDate.getMonth()];
+    var day = fullDate.getDate();
+    var hours = fullDate.getHours();
+    var mins = fullDate.getMinutes();
+
+
+
+    if (hours < 10){
+        hours = "0" + hours;
     }
 
-        function setLocation(long, lat) {
-            const proxy = "https://cors-anywhere.herokuapp.com/";
-            const apiLocation = `${proxy}https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=AIzaSyDgkz55qvQ6bG36xW7MUjY0ORVDYqCi-Mw`
-            fetch(apiLocation)
-                .then (fetched => {
-                    return fetched.json();
-                })
-                .then(dataL => {
-                    console.log(dataL);
-                    locationArray = dataL.results[0];
-                    for (var i=0; i<locationArray.address_components.length; i++)
-                    {
-                        if (locationArray.address_components[i].types[0] == "locality"){
-                            city = locationArray.address_components[i];
-                        }
-                        if (locationArray.address_components[i].types[0] == "administrative_area_level_1"){
-                            region = locationArray.address_components[i];
-                        }
-                        if (locationArray.address_components[i].types[0] == "postal_code"){
-                            postal = locationArray.address_components[i];
-                        }
-                    }
-                    locationTimezone.textContent = city.long_name + ", " + region.long_name;
-                })
+    if (mins < 10) {
+        mins = "0" +mins;
+    }
 
+    document.getElementById('dow').innerHTML = d + ",";
+    document.getElementById('month').innerHTML = m;
+    document.getElementById('day').innerHTML = day;
+    document.getElementById('hour').innerHTML = hours + " :";
+    document.getElementById('minute').innerHTML = mins;
+
+    if (hours > 19 || hours < 7){
+        document.body.style.background ='linear-gradient(rgb(89,0,179), rgb(38,0,77))';
+    }
+
+}
+
+//Possible in the future to reset Geolocation Denial on button click.
+/*function handlePermission(){
+    navigator.permissions.query({name: "geolocation"}).then(function(result){
+        if (result.state == 'granted'){
+            report(result.state);
+            geoBtn.style.display = 'none';
+        } else if (result.state == 'prompt'){
+            report(result.state);
+            geoBtn.style.display = 'none';
+            navigator.geolocation.getCurrentPosition(revealPosition,positionDenied,geoSettings);
+        } else if (result.state == 'denied'){
+            report(result.state);
+            geoBtn.style.display = 'inline';
         }
-
-        function setClock(tz){
-            console.log(tz);
-            const dateString = new Date().toLocaleString("en-US", {timeZone: `${tz}`});
-            const fullDate = new Date(dateString);
-            console.log(dateString);
-            console.log(fullDate);
-
-            var dow = new Array();
-            dow[0] = "Sunday";
-            dow[1] = "Monday";
-            dow[2] = "Tuesday";
-            dow[3] = "Wednesday";
-            dow[4] = "Thursday";
-            dow[5] = "Friday";
-            dow[6] = "Saturday";
-            var d = dow[fullDate.getDay()];
-            var month = new Array();
-            month[0] = "January";
-            month[1] = "February";
-            month[2] = "March";
-            month[3] = "April";
-            month[4] = "May";
-            month[5] = "June";
-            month[6] = "July";
-            month[7] = "August";
-            month[8] = "September";
-            month[9] = "October";
-            month[10] = "Novemeber";
-            month[11] = "December";
-            var m = month[fullDate.getMonth()];
-            var day = fullDate.getDate();
-            var hours = fullDate.getHours();
-            var mins = fullDate.getMinutes();
-
-
-
-            if (hours < 10){
-                hours = "0" + hours;
-            }
-        
-            if (mins < 10) {
-                mins = "0" +mins;
-            }
-
-            document.getElementById('dow').innerHTML = d + ",";
-            document.getElementById('month').innerHTML = m;
-            document.getElementById('day').innerHTML = day;
-            document.getElementById('hour').innerHTML = hours + " :";
-            document.getElementById('minute').innerHTML = mins;
-
-            if (hours > 19 || hours < 7){
-                document.body.style.background ='linear-gradient(rgb(89,0,179), rgb(38,0,77))';
-            }
-
+        result.onchange = function() {
+            report(result.state);
         }
+    });
+}*/
 
-        function setIcons(icon, iconID) {
-            const skycons = new Skycons({color: "white"});
-            const currentIcon = icon.replace(/-/g, "_").toUpperCase();
-            skycons.play();
-            return skycons.set(iconID, Skycons[currentIcon]);
-        }      
-});
+function report(state) {
+    console.log('Permission ' + state);
+}
+
+window.addEventListener('load', ()=>{
+    getLocation();
+    setLocation();
+})
+
+locationButton.addEventListener("click", ()=>{
+    clearInterval(setClock);
+    getLocation();
+    setLocation();
+})
